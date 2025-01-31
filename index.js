a4593725a5ec42cdbc9e78e4ac7b1a5e5716e624
Quinn Michaels <quinn@indra.ai>
2025-01-30 19:57:08 -0800

    update content structures.

---
+++ index.js
@@ -0,0 +1,49 @@
+// Copyright (c)2025 Quinn Michaels
+// Content Deva
+// Manages various Content in Deva.space, Deva.cloud, and Deva.world.
+import Deva from '@indra.ai/deva';
+import pkg from './package.json' with {type:'json'};
+
+import data from './data.json' with {type:'json'};
+const {agent,vars} = data.DATA;
+
+// set the __dirname
+import {dirname} from 'node:path';
+import {fileURLToPath} from 'node:url';    
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const info = {
+	id: pkg.id,
+	name: pkg.name,
+	describe: pkg.description,
+	version: pkg.version,
+	dir: __dirname,
+	url: pkg.homepage,
+	git: pkg.repository.url,
+	bugs: pkg.bugs.url,
+	author: pkg.author,
+	license: pkg.license,
+	copyright: pkg.copyright,
+};
+const CONTENT = new Deva({
+	info,
+	agent,
+	vars,
+	utils: {
+		translate(input) {return input.trim();},
+		parse(input) {return input.trim();},
+		proecess(input) {return input.trim();}
+	},
+	listeners: {},
+	modules: {},
+	func: {},
+	methods: {},
+	onReady(data, resolve) {
+		this.prompt('ready');
+		return resolve(data);
+	},
+	onError(err) {
+		console.log('ERR', err);
+	}
+});
+export default CONTENT
